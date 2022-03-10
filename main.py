933ba767eddd02d41764020df643bbc1cde99308
Chani-git <chanish42@gmail.com>
2022-03-10 20:14:13 +0300

    Тест состояний

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import telebot
 from telebot import types
 import asana
 import config
+import dbworker
 from common import AsanaTicket
+
+
 
 Token = config.Token
 AsanaToken = config.AsanaToken
@@ -19,6 +22,11 @@ client = asana.Client.access_token(AsanaToken)
 
 task_in_work = {}
 
+@bot.message_handler(commands=["reset"])
+def cmd_reset(message):
+    bot.send_message(message.chat.id, hi_msg)
+    dbworker.set_state(message.chat.id, config.States.S_START.value)
+
 @bot.message_handler(commands=['start'])
 def start_message(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
@@ -31,6 +39,19 @@ def start_message(message):
 
 @bot.message_handler(content_types=['text'])
 def bot_message(message):
+    state = dbworker.get_current_state(message.chat.id)
+    if state == config.States.S_ENTER_HOSTEL.value:
+        bot.send_message(message.chat.id,
+                         "Вы не указали номер общежития")
+    elif state == config.States.S_ENTER_ROOM.value:
+        bot.send_message(message.chat.id,
+                         "Вы не указали свою комнату")
+    elif state == config.States.S_ENTER_TROUBLE.value:
+        bot.send_message(message.chat.id,
+                         "Вы не описали свою проблему")
+    else:  # Под "остальным" понимаем состояние "0" - начало диалога
+        bot.send_message(message.chat.id, hi_msg)
+        dbworker.set_state(message.chat.id, config.States.S_ENTER_HOSTEL.value)
     if message.chat.type == 'private':
         """"
         if message.text == 'Инструкции':
@@ -44,19 +65,25 @@ def bot_message(message):
             bot.send_message(message.chat.id, 'Введите номер общежития \n\n Input hostel number')
             bot.register_next_step_handler(message, call_hostel)
 
-
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_HOSTEL.value)
 def call_hostel(message):
-    task_in_work[message.chat.id].hostel = message.text
-    bot.send_message(message.from_user.id, "Введите номер комнаты\n\n Input room number")
-    bot.register_next_step_handler(message, call_room)
+    if not message.text in ['4','5','6','8','9']:
+        bot.send_message(message.chat.id, "Что-то не так, попробуй ещё раз!")
+        return
+    else:
+        task_in_work[message.chat.id].hostel = message.text
+        bot.send_message(message.from_user.id, "Введите номер комнаты\n\n Input room number")
+        bot.register_next_step_handler(message, call_room)
+        dbworker.set_state(message.chat.id, config.States.S_ENTER_ROOM.value)
 
-
-def call_room(message, ):
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_ROOM.value)
+def call_room(message):
     task_in_work[message.chat.id].room = message.text
     bot.send_message(message.from_user.id, "Опишите свою проблему\n\nAsk you question")
     bot.register_next_step_handler(message, call_trouble)
+    dbworker.set_state(message.chat.id, config.States.S_ENTER_TROUBLE.value)
 
-
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_TROUBLE.value)
 def call_trouble(message):
     task_in_work[message.chat.id].trouble = message.text + '\n' + 'https://t.me/' + str(message.from_user.username)
 
